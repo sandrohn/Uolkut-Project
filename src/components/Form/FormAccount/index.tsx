0bba282343cdaf8d7354d0df5190a0ed68963678
Sandro Henrique <sandrohenriquens@gmail.com>
2023-08-22 17:23:15 -0300

    style: updated uol layout

--- src/components/Form/FormAccount/index.tsx
+++ src/components/Form/FormAccount/index.tsx
@@ -13,52 +13,39 @@ export const FormAccount = () => {
     <Form onSubmit={handleFormSubmit} className={styles.formContainer}>
       <fieldset>
         <p>
-          <label htmlFor="name">Nome</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="João da Silva Bernardo"
-            required
-          />
+          <input type="email" id="email" placeholder="Email" required />
         </p>
         <p>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="exemplo@email.com"
-            required
-          />
-        </p>
-        <p>
-          <label htmlFor="date">Data de Nascimento</label>
-          <input type="date" id="date" required />
-        </p>
-        <p>
-          <label htmlFor="password" id="city-state">
-            Cidade / Estado
-          </label>
-          <input type="text" id="city-state" placeholder="Recife-PE" required />
-        </p>
-        <p>
-          <label htmlFor="password">Senha</label>
           <input
             type="password"
             id="password"
-            placeholder="********"
+            placeholder="Senha"
             minLength={8}
             required
           />
         </p>
+
         <p>
-          <label htmlFor="confirm-password">Confirmar Senha</label>
-          <input
-            type="password"
-            id="confirm-password"
-            placeholder="********"
-            required
-            minLength={8}
-          />
+          <input type="text" id="date" placeholder="DD/MM/AAAA" required />
+        </p>
+        <p>
+          <input type="text" id="job" placeholder="Profissão" required />
+        </p>
+        <p>
+          <input type="text" id="ccity" placeholder="País" required />
+        </p>
+        <p>
+          <input type="text" id="country" placeholder="Cidade" required />
+        </p>
+        <p>
+          <select required>
+            <option disabled selected value="">
+              Relacionamento
+            </option>
+            <option>Solteiro</option>
+            <option>Casado</option>
+            <option>Viúvo</option>
+          </select>
         </p>
       </fieldset>
 
