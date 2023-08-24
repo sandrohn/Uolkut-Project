0e265e87a154fb408f3711a87ad0663151a03e95
Sandro Henrique <sandrohenriquens@gmail.com>
2023-08-23 22:22:50 -0300

    style: reformed forms

--- src/components/Form/FormLogin/index.tsx
+++ src/components/Form/FormLogin/index.tsx
@@ -1,7 +1,20 @@
 import { Form, Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+
+import { auth } from "../../../services/firebaseConfig.js";
+
 import styles from "./FormLogin.module.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export const FormLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // signInWithEmailAndPassword{auth, email, password}
+  };
+
   const navigate = useNavigate();
 
   const handleFormSubmit = (event: React.SyntheticEvent) => {
@@ -13,10 +26,22 @@ export const FormLogin = () => {
     <Form onSubmit={handleFormSubmit} className={styles.formLoginContainer}>
       <fieldset>
         <p>
-          <input type="email" placeholder="E-mail" required />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </p>
         <p>
-          <input type="password" placeholder="Senha" required />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </p>
         <p className={styles.checkboxContainer}>
           <input type="checkbox" name="remember-password" />
@@ -25,7 +50,9 @@ export const FormLogin = () => {
       </fieldset>
 
       <fieldset>
-        <button className={styles.button_login}>Entrar na conta</button>
+        <button className={styles.button_login} type="submit">
+          Entrar na conta
+        </button>
 
         <Link to="/register">
           <button className={styles.button_register}>Criar uma conta</button>
